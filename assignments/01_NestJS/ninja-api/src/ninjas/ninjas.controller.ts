00385de1a79da2784a10324029d5b9af7e6a2471
Domen <domen.gnezda@pm.me>
2023-11-22 16:33:27 +0100

    Update ninjas.controller.ts

--- assignments/01_NestJS/ninja-api/src/ninjas/ninjas.controller.ts
+++ assignments/01_NestJS/ninja-api/src/ninjas/ninjas.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+    // GET /ninjas --> []
+    @Get()
+    getNinjas() {
+        return [];
+    }
+
+    // GET /ninjas/:id --> { ... }
+    @Get(':id')
+    getOneNinja() {
+        return {};
+    }
+    
+    // POST /ninjas
+    // PUT /ninjas/:id --> { ... }
+    // DELETE /ninjas/:id
+}
+
+
