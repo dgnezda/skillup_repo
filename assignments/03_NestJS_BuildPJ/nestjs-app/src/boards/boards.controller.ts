516d1a3e8d77ef78a14720a5a976565e3e7792ec
Domen <domen.gnezda@pm.me>
2023-11-25 12:39:21 +0100

    Add delete, patch service & ctrl

--- assignments/03_NestJS_BuildPJ/nestjs-app/src/boards/boards.controller.ts
+++ assignments/03_NestJS_BuildPJ/nestjs-app/src/boards/boards.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { BoardsService } from './boards.service';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
 
 @Controller('boards')
-export class BoardsController {
+export class BoardsController { 
     constructor(private boardsService: BoardsService) {} // private keyword turns parameter 'boardsService' into a class property so it can be accessed elsewhere
 
     @Get()
@@ -12,8 +12,25 @@ export class BoardsController {
         return this.boardsService.getAllBoards();
     }
 
+    @Get('/:id')
+    getBoardById(@Param('id') id: string): Board {
+        return this.boardsService.getBoardById(id);
+    }
+
     @Post()
     createBoard(@Body() createBoardDto: CreateBoardDto) :Board {
         return this.boardsService.createBoard(createBoardDto);
+    }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id: string): void {
+        this.boardsService.deleteBoard(id);
+    }
+
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id') id: string,
+        @Body('status') status: BoardStatus,) {
+            return this.boardsService.updateBoardStatus(id, status);
     }
 }
