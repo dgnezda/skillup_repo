b1dc55bdc3cbe91bd65beed8ce8436d82cfd1935
Domen <domen.gnezda@pm.me>
2023-11-26 19:02:40 +0100

    Add save users

--- assignments/03_NestJS_BuildPJ/nestjs-app/src/auth/auth.controller.ts
+++ assignments/03_NestJS_BuildPJ/nestjs-app/src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth.credential.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+    constructor(
+        private authService: AuthService
+    ) {}
+
+    @Post('/signup')
+    signUp(@Body(ValidationPipe) authCredentials: AuthCredentialsDto): Promise<void> {
+        return this.authService.signUp(authCredentials);
+    }
+}
--- assignments/03_NestJS_BuildPJ/nestjs-app/src/auth/user.repository.ts
+++ assignments/03_NestJS_BuildPJ/nestjs-app/src/auth/user.repository.ts
@@ -1,7 +1,24 @@
 import { EntityRepository, Repository } from "typeorm";
 import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./dto/auth.credential.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User) // TODO: depracated decorator
 export class UserRepository extends Repository<User> {
-
+    
+    async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        const { username, password } = authCredentialsDto;
+        const user = this.create({ username, password });
+        try {
+            await this.save(user);
+        } catch (error) {
+            console.log('error', error) // to log error number (check it)
+            // throw new Exception
+            if (error.code === "23505") {
+                throw new ConflictException(`Sorry, username "${username}" is already taken.`);
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
 }
